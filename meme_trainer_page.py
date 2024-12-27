eea65f846ec5c7f545ebda9cd37bdf6721ff8316
Adrien-Dugast <adrien.dugast@ensta-paris.fr>
2024-12-28 00:19:40 +0100

    adding videos

--- meme_trainer_page.py
+++ meme_trainer_page.py
@@ -1,29 +1,15 @@
 import streamlit as st
 import webbrowser
 import random
-
-def charge_list_refs():
-    filename = "liste_refs.txt"
-    f = open(filename, "r")
-    lines = f.read().split(sep = '\n')
-    list_refs = [string.split(sep = ', ')[0] for string in lines]
-
-    return(list_refs)
+import os
 
 def display_random_video(list_refs):
-    st.text('coucou 1')
-    if len(list_refs)==0:
-        list_refs = charge_list_refs()
-        if len(list_refs)==0:
-            st.text("oups aucune ref dans la liste, c'est un bug...")
-    st.text('coucou 2')
-    url = random.choice(list_refs)
-    list_refs.remove(url)
-    webbrowser.open_new_tab(url)
-    st.text('coucou 3')
-    return(list_refs)
+    list_vids = os.listdir('videos')
+    vid_to_display = random.choice(list_vids)
+    video_file = open("Jeux1.mp4", "rb")
+    video_bytes = video_file.read()
+    st.video(video_bytes, autoplay = True)
 
-list_refs = charge_list_refs()
 
 st.title('Meme trainer 3000 GX premium edition')
 
@@ -38,11 +24,5 @@ a = st.button("Je veux devenir quelqu'un de cultivé et intéressant en sociét�
 
 
 if a:
-    st.text('Bouton appuyé')
-    st.text(f"{len(list_refs)} refs dans la liste")
-    video_file = open("Jeux1.mp4", "rb")
-    video_bytes = video_file.read()
-    st.video(video_bytes, autoplay = True)
-
-    list_refs = display_random_video(list_refs)
+    display_random_video()
     
