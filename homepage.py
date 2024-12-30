983f94c24cd3eb267bc1a935d0d0e2cdd9ffbf25
Adrien-Dugast <adrien.dugast@ensta-paris.fr>
2024-12-30 21:34:50 +0100

    trying to log things

--- homepage.py
+++ homepage.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 
 st.title("Bienvenue sur la page d'Adrien")
 st.header("A gauche la liste de mes apps rigolotes (en construction)")
@@ -7,3 +8,7 @@ st.text('''
         J'ai créé ce site pour mettre un peu toutes mes conneries, je sais pas à quel point ça va 
         marcher ni à quel point je vais en mettre mais bon ça m'amuse donc je le fais.
         ''')
+
+f = open("log.txt", 'a')
+f.write(datetime.datetime.now())
+f.close()
